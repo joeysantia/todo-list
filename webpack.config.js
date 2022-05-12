54ff97ea62737f7f98a3458abcbb84ddbfddd6c1
Joey Santia <joey.santia@gmail.com>
2022-05-12 12:19:30 -0400

    Update image rules for webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,11 @@ module.exports = {
             {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader']
-            }
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource'
+            },
         ]
     },
     output: {
